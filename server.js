a2dea3d08f68a54ff2ea7e7173ee7c4def58f862
Glitch (emma-server) <none>
2019-12-15 22:58:20 +0000

    added morphine

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.route('/').get((req, res) => {
   res.json({Compendium: Compendium, Conditions: Conditions});
 });
 
+app.route('/privacy').get((req, res) => {
+  res.send(
+    "Emma stores all the information you provide to her on your phone. Therefore, this information is only as secure as your phone. It is your responsibility to keep your phone safe. " + 
+    "Emma does not transmit this information anywhere else except when you ask her to open up your phone's gmail account to pre-fill the email body. Even then, you must input the email address and hit 'send' before any information is sent. " +
+    "Emma does not communicate with external devices except when you touch the 'Update' button to update her clinical knowledge. This sends a request to pull information from a server. No health information is transmitted from your phone in this request."
+  );
+});
+
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
 });
